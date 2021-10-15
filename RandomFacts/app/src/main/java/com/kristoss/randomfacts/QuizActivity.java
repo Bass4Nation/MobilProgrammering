c91474d668385d4cc73e9bb53e5224f111cc30ec
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2021-10-15 18:52:07 +0200

    fikset nav i quiz

--- RandomFacts/app/src/main/java/com/kristoss/randomfacts/QuizActivity.java
+++ RandomFacts/app/src/main/java/com/kristoss/randomfacts/QuizActivity.java
@@ -1,20 +1,30 @@
 package com.kristoss.randomfacts;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class QuizActivity extends AppCompatActivity {
+public class QuizActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     Button btnTrue, btnFalse, btnNext;
     TextView question, answer;
+    DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +74,26 @@ public class QuizActivity extends AppCompatActivity {
             startActivity(myInt);
         }));
 
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null) { // Rotate
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FrontpageActivity()).commit();
+            navigationView.setCheckedItem(R.id.nav_frontpage);
+        }
+
+
     }
 
     public String answerCheck(String answer, String chosen){
@@ -75,4 +105,44 @@ public class QuizActivity extends AppCompatActivity {
         }
        return valuation;
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent frontpage = new Intent(getApplicationContext(), FrontpageActivity.class);
+        Intent quiz = new Intent(getApplicationContext(), QuizActivity.class);
+        Intent doc = new Intent(getApplicationContext(), FrontpageActivity.class);
+
+
+        switch (item.getItemId()){
+            case R.id.nav_frontpage:
+                startActivity(frontpage);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FrontPageFragment()).commit();
+
+                break;
+            case R.id.nav_documents:
+                startActivity(quiz);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DocumentFragment()).commit();
+                break;
+            case R.id.nav_quiz:
+                startActivity(quiz);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new QuizFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
 }
