fefd3fdc6a8b94e0d8991f8257b34cc37e269d51
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2021-12-11 15:20:45 +0100

    Startet med fragment convertering

---
+++ RandomFacts/app/src/main/java/com/kristoss/randomfacts/FrontpageFragment.java
@@ -0,0 +1,12 @@
+package com.kristoss.randomfacts;
+
+import androidx.fragment.app.Fragment;
+
+public class FrontpageFragment extends Fragment {
+    public FrontpageFragment() {
+        super(R.layout.activity_frontpage);
+    }
+}
+
+//LINKER
+// https://developer.android.com/guide/fragments/create
